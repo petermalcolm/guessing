70a92c98f195b5dc542756c9ef283d639cb17ce4
petermalcolm <peter.t.malcolm@gmail.com>
2017-11-12 20:38:48 -0500

    refactor to OO

--- main.rb
+++ main.rb
@@ -1,25 +1,35 @@
-def handle_input(input)
-  result = eval(input)
-  puts(" => #{result}")
-end
 
 repl = -> prompt do
-  qs = {
-  	:"Does it have hair?"             => { :"mouse" => true,  :"cat" => true,  :"trout" => false},
-  	:"Does it prey on other animals?" => { :"mouse" => false, :"cat" => true,  :"trout" => false},
-  	:"Does it fit in a shoebox?"      => { :"mouse" => true,  :"cat" => false, :"trout" => true },
-  }
   # puts qs[:"Does it prey on other animals?"][:"mouse"]
   print "are two mice equal? "
   puts qs.values[0].keys[0] === qs.values[1].keys[0]
 
   print "does a mouse equal a trout? "
   puts qs.values[0].keys[0] === qs.values[1].keys[2]
-
-  print prompt
-  handle_input(gets.chomp!)
 end
 
-loop do
-  repl[">> "]
+
+class Guessing
+	def initialize()
+		qs = {
+			:"Does it have hair?"             => { :"mouse" => true,  :"cat" => true,  :"trout" => false},
+			:"Does it prey on other animals?" => { :"mouse" => false, :"cat" => true,  :"trout" => false},
+			:"Does it fit in a shoebox?"      => { :"mouse" => true,  :"cat" => false, :"trout" => true },
+		}
+		main_loop()
+	end
+
+	def main_loop()
+		loop do
+			print " >>"
+			handle_input(gets.chomp!)
+		end
+	end
+
+	def handle_input(input)
+		result = eval(input)
+		puts(" => #{result}")
+	end
 end
+
+Guessing.new
