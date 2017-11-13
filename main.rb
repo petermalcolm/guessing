a328c0ac06164853d6c0f690f7b6ecd30656454f
petermalcolm <peter.t.malcolm@gmail.com>
2017-11-12 21:28:01 -0500

    basic repl set up

--- main.rb
+++ main.rb
@@ -11,24 +11,49 @@ end
 
 class Guessing
 	def initialize()
-		qs = {
+		@qs = {
 			:"Does it have hair?"             => { :"mouse" => true,  :"cat" => true,  :"trout" => false},
 			:"Does it prey on other animals?" => { :"mouse" => false, :"cat" => true,  :"trout" => false},
 			:"Does it fit in a shoebox?"      => { :"mouse" => true,  :"cat" => false, :"trout" => true },
 		}
-		main_loop()
+		qs_filtered = qs_clone(@qs)
+		puts @qs
+		puts '- - -'
+		puts qs_filtered
+		puts '- - -'
+		main_loop(qs_filtered)
 	end
 
-	def main_loop()
+	def qs_clone(qs) # deep clone
+		_clone = Hash.new
+		qs.each do |q,animals|
+			_animals_clone = Hash.new
+			animals.each do |animal,bool|
+				_animals_clone[animal] = bool
+			end
+			_clone[q] = _animals_clone
+		end
+		return _clone
+	end
+
+	def main_loop(qs_filtered) # repl
 		loop do
-			print " >>"
-			handle_input(gets.chomp!)
+			q = qs_filtered.keys.sample
+			print q
+			input = get_input
 		end
 	end
 
-	def handle_input(input)
-		result = eval(input)
-		puts(" => #{result}")
+	def get_input
+		input = ''
+		while input != 'y' and input != 'n'
+			print " y / n : "
+			input = gets.downcase.chomp!
+			if input != 'y' and input != 'n'
+				puts 'I did not get that, sorry.'
+			end
+		end 
+		return input
 	end
 end
 
